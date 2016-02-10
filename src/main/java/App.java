a94fa008aa831c93585f39b6927efd1e48aa0163
Brad London and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-02-10 10:06:02 -0800

    write checkWinner method with passing tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,3 +3,40 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+
+  }
+
+  public static String checkWinner(String player1, String player2) {
+    String winner = "";
+    String tie = "Tie";
+    if (player1.equals("Rock")) {
+      if (player2.equals("Scissors")) {
+        winner = player1;
+      } else if (player2.equals("Paper")) {
+        winner = player2;
+      } else {
+        winner = tie;
+      }
+    } else if (player1.equals("Scissors")) {
+      if (player2.equals("Paper")) {
+        winner = player1;
+      } else if (player2.equals("Rock")) {
+        winner = player2;
+      } else {
+        winner = tie;
+      }
+    } else {
+      if (player2.equals("Rock")) {
+        winner = player1;
+      } else if (player2.equals("Scissors")) {
+        winner = player2;
+      } else {
+        winner = tie;
+      }
+    }
+    return winner;
+  }
+}  
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,18 +7,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest extends FluentTest {
-  public WebDriver webDriver = new HtmlUnitDriver();
-  public WebDriver getDefaultDriver() {
-      return webDriver;
-  }
+// public class AppTest extends FluentTest {
+//   public WebDriver webDriver = new HtmlUnitDriver();
+//   public WebDriver getDefaultDriver() {
+//       return webDriver;
+//   }
+//
+//   @ClassRule
+//   public static ServerRule server = new ServerRule();
+//
+//   @Test
+//   public void rootTest() {
+//       goTo("http://localhost:4567/");
+//       assertThat(pageSource()).contains("");
+//   }
+// }
 
-  @ClassRule
-  public static ServerRule server = new ServerRule();
+public class AppTest {
+  @Test
+  public void checkWinner_rockBeatsScissors_rockwins() {
+    App testApp = new App();
+    assertEquals("Rock", testApp.checkWinner("Rock", "Scissors"));
+  }
 
   @Test
-  public void rootTest() {
-      goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("");
+  public void checkWinner_paperWin_paperWins() {
+    App testApp = new App();
+    assertEquals("Paper", testApp.checkWinner("Rock", "Paper"));
   }
+
+  @Test
+  public void checkWinner_rockVSrock_tie() {
+    App testApp = new App();
+    assertEquals("Tie", testApp.checkWinner("Rock", "Rock"));
+  }
+
+  @Test
+  public void checkWinner_ScissorsBeatsPaper_rockwins() {
+    App testApp = new App();
+    assertEquals("Scissors", testApp.checkWinner("Scissors", "Paper"));
+  }
+
+  @Test
+  public void checkWinner_ScissorslosesToRock_rockwins() {
+    App testApp = new App();
+    assertEquals("Rock", testApp.checkWinner("Scissors", "Rock"));
+  }
+
 }
